afe787c161b961b2c95bd164436ddc1177df48b1
lvfonsecaa <lv.fonseca@uniandes.edu.co>
2025-04-04 19:20:26 -0500

    punto 2

--- main.ts
+++ main.ts
@@ -1,18 +1,27 @@
-    import { Serie } from './Serie.js';
-    import { series } from './data.js';
+import { Serie } from './Serie.js';
+import { series } from './data.js';
 
-    let seriesTbody: HTMLElement = document.getElementById('series')!;
+let seriesTbody: HTMLElement = document.getElementById('series')!;
+const promedioDiv: HTMLElement = document.getElementById('promedio')!;
 
-    renderSeriesInTable(series);
+renderSeriesInTable(series);
 
-    function renderSeriesInTable(series: Serie[]): void {
-    console.log('Desplegando series');
-    series.forEach((serie) => {
-        let trElement = document.createElement("tr");
-        trElement.innerHTML = `<td>${serie.id}</td>
-                            <td>${serie.nombre}</td>
-                            <td>${serie.canal}</td>
-                            <td>${serie.temporadas}</td>`;
-        seriesTbody.appendChild(trElement);
-    });
-    }
+promedioDiv.innerHTML = `Promedio de temporadas: ${calcularPromedioTemporadas(series)}`;
+
+function renderSeriesInTable(series: Serie[]): void {
+  console.log('Desplegando series');
+  series.forEach((serie) => {
+    let trElement = document.createElement("tr");
+    trElement.innerHTML = `<td>${serie.id}</td>
+                           <td>${serie.nombre}</td>
+                           <td>${serie.canal}</td>
+                           <td>${serie.temporadas}</td>`;
+    seriesTbody.appendChild(trElement);
+  });
+}
+
+function calcularPromedioTemporadas(series: Serie[]): number {
+  let totalSeasons = 0;
+  series.forEach((serie) => totalSeasons += serie.temporadas);
+  return totalSeasons / series.length;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,18 @@
 import { series } from './data.js';
 var seriesTbody = document.getElementById('series');
+var promedioDiv = document.getElementById('promedio');
 renderSeriesInTable(series);
+promedioDiv.innerHTML = "Promedio de temporadas: ".concat(calcularPromedioTemporadas(series));
 function renderSeriesInTable(series) {
     console.log('Desplegando series');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                            <td>").concat(serie.nombre, "</td>\n                            <td>").concat(serie.canal, "</td>\n                            <td>").concat(serie.temporadas, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td>").concat(serie.nombre, "</td>\n                           <td>").concat(serie.canal, "</td>\n                           <td>").concat(serie.temporadas, "</td>");
         seriesTbody.appendChild(trElement);
     });
+}
+function calcularPromedioTemporadas(series) {
+    var totalSeasons = 0;
+    series.forEach(function (serie) { return totalSeasons += serie.temporadas; });
+    return totalSeasons / series.length;
 }
