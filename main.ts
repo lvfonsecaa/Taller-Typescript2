9e685ab67bf2a317279fc9cb58b3ef216c286d94
lvfonsecaa <lv.fonseca@uniandes.edu.co>
2025-04-06 18:15:46 -0500

    no sirven las imagenes

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { series } from './data.js';
 
 let seriesTbody: HTMLElement = document.getElementById('series')!;
 const promedioDiv: HTMLElement = document.getElementById('promedio')!;
+const detalleContainer: HTMLElement = document.getElementById('detalles')!;
 
 renderSeriesInTable(series);
 
@@ -13,15 +14,46 @@ function renderSeriesInTable(series: Serie[]): void {
   series.forEach((serie) => {
     let trElement = document.createElement("tr");
     trElement.innerHTML = `<td>${serie.id}</td>
-                           <td>${serie.nombre}</td>
+                           <td><a href="#" class="serie-link" data-id="${serie.id}">${serie.nombre}</a></td>
                            <td>${serie.canal}</td>
                            <td>${serie.temporadas}</td>`;
     seriesTbody.appendChild(trElement);
   });
+  document.querySelectorAll('.serie-link').forEach(link => 
+    {
+      link.addEventListener('click', (e) => 
+      {
+        e.preventDefault();
+        const id = Number((e.target as HTMLElement).getAttribute('data-id'));
+        let selectedSerie: Serie;
+        
+        for (let i = 0; i < series.length; i++) 
+        {
+          if (series[i].id === id) 
+          {
+            selectedSerie = series[i];
+            showSerieDetail(selectedSerie);
+            break;
+          }
+        }
+      });
+    });
 }
 
 function calcularPromedioTemporadas(series: Serie[]): number {
   let totalSeasons = 0;
   series.forEach((serie) => totalSeasons += serie.temporadas);
   return totalSeasons / series.length;
+}
+
+
+function showSerieDetail(serie: Serie): void {
+  detalleContainer.innerHTML = `
+    <img class="card-img-top" src="${serie.imagen}" alt="${serie.nombre}">
+    <div class="card-body">
+      <h5 class="card-title">${serie.nombre}</h5>
+      <p class="card-text">${serie.sinopsis}</p>
+      <a href="${serie.link}" target="_blank" class="card-link">${serie.link}</a>
+    </div>
+  `;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,36 @@
 import { series } from './data.js';
 var seriesTbody = document.getElementById('series');
 var promedioDiv = document.getElementById('promedio');
+var detalleContainer = document.getElementById('detalles');
 renderSeriesInTable(series);
 promedioDiv.innerHTML = "Promedio de temporadas: ".concat(calcularPromedioTemporadas(series));
 function renderSeriesInTable(series) {
     console.log('Desplegando series');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td>").concat(serie.nombre, "</td>\n                           <td>").concat(serie.canal, "</td>\n                           <td>").concat(serie.temporadas, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td><a href=\"#\" class=\"serie-link\" data-id=\"").concat(serie.id, "\">").concat(serie.nombre, "</a></td>\n                           <td>").concat(serie.canal, "</td>\n                           <td>").concat(serie.temporadas, "</td>");
         seriesTbody.appendChild(trElement);
+    });
+    document.querySelectorAll('.serie-link').forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            var id = Number(e.target.getAttribute('data-id'));
+            var selectedSerie;
+            for (var i = 0; i < series.length; i++) {
+                if (series[i].id === id) {
+                    selectedSerie = series[i];
+                    showSerieDetail(selectedSerie);
+                    break;
+                }
+            }
+        });
     });
 }
 function calcularPromedioTemporadas(series) {
     var totalSeasons = 0;
     series.forEach(function (serie) { return totalSeasons += serie.temporadas; });
     return totalSeasons / series.length;
+}
+function showSerieDetail(serie) {
+    detalleContainer.innerHTML = "\n    <img class=\"card-img-top\" src=\"".concat(serie.imagen, "\" alt=\"").concat(serie.nombre, "\">\n    <div class=\"card-body\">\n      <h5 class=\"card-title\">").concat(serie.nombre, "</h5>\n      <p class=\"card-text\">").concat(serie.sinopsis, "</p>\n      <a href=\"").concat(serie.link, "\" target=\"_blank\" class=\"card-link\">").concat(serie.link, "</a>\n    </div>\n  ");
 }
